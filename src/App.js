dc6a33c537d045c12d69fe0a1ea1a4e622624f8e
ristmi <ristmidaffahoiri@gmail.com>
2022-06-14 17:48:09 +0700

    fix minor

--- src/App.js
+++ src/App.js
@@ -1,58 +1,65 @@
 import "./App.css";
 
 import { useState, useEffect } from "react";
-import "./Style/Film.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, Button, Navbar, Container} from"react-bootstrap"
+import "./Style/Film.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Button, Navbar, Container } from "react-bootstrap";
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  const [item, setItem] = useState({});
   const [inputId, setInputId] = useState("");
 
   function getData() {
     fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=3fa6cf92&t=${inputId}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
+      .then((response) => response.json())
+      .then((json) => setItem(json));
   }
 
   function resetData() {
-    setItems([]);
+    setItem({});
     setInputId("");
   }
 
   useEffect(() => {
     fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=3fa6cf92`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-  }, [])
+      .then((response) => response.json())
+      .then((json) => setItem(json));
+  }, []);
 
-  let arr = []
-  arr.push(items)
-  console.log(items.Title, "<<< ARR")
   return (
-
-    <div className="App">   
-     <Navbar bg="dark" variant="dark" width="10%" height="10%">
-    <Container>
-    <Navbar.Brand href="#home">RISTMI</Navbar.Brand>
-    </Container>
-  </Navbar>
-  <br/>
-    <h1 style={{color: 'white'}}>CARI FILM</h1>
+    <div className="App">
+      <Navbar bg="dark" variant="dark" width="10%" height="10%">
+        <Container>
+          <Navbar.Brand href="#home">RISTMI</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      <h1 style={{ color: "white" }}>CARI FILM</h1>
       <input value={inputId} onChange={(e) => setInputId(e.target.value)} />
       <button onClick={() => getData()}>cari</button>
       <button onClick={() => resetData()}>reset</button>
 
-      <div>
+      {Object.keys(item).length !== 0 && (
+        <div>
           <div>
-
-          <img style={{width: '20%', height: '20%', marginLeft: '13px', marginBottom: '20px', marginTop: '20px', position: 'center'}} src={items.Poster} ></img>
-         <Container >
-          <h4 style={{color: 'white'}}>{items.Title}</h4>
-          <p style={{color: 'white'}}>{items.Plot}</p>
-          </Container>
+            <img
+              style={{
+                width: "20%",
+                height: "20%",
+                marginLeft: "13px",
+                marginBottom: "20px",
+                marginTop: "20px",
+                position: "center",
+              }}
+              src={item.Poster}
+            ></img>
+            <Container>
+              <h4 style={{ color: "white" }}>{item.Title}</h4>
+              <p style={{ color: "white" }}>{item.Plot}</p>
+            </Container>
           </div>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
